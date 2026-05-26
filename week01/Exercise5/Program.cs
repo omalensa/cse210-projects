0c9dcec077119b00a3e6e076fec3c4d07df12d16
omalensa <s.omales23@gmail.com>
2026-05-27 00:18:42 +0100

    completed example 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        //Main: 
+        DisplayWelcomemess();
+        string userName = getusername();
+        int userNumber = getuserNumber();
+        int square = getsquare(userNumber);
+
+        DisplayResult(userName, square);
+
+
+    }
+    // Prints the welcome message
+    static void DisplayWelcomemess()
+    {
+        Console.WriteLine("Welcome to the my programme");
+    }
+    //Gets the user's name
+    static string getusername()
+    {
+        Console.Write("Please enter your Name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    // Gets number input from the user
+    static int getuserNumber()
+    {
+        Console.Write("Please enter your preferred number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+    // Calculates the square
+    static int getsquare(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string userName, int square)
+    {
+        Console.WriteLine($"{userName}, the square of your number is: {square}");
     }
 }
