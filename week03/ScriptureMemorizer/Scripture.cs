1a20fc83356c60cdcae09cc47d6acf23f19d5126
omalensa <s.omales23@gmail.com>
2026-05-30 11:23:16 +0100

    completed week 3

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        foreach (string word in text.Split(" "))
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public void HideRandomWords(int count)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < count; i++)
+        {
+            int index = rand.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string scriptureText = "";
+        foreach (Word word in _words)
+        {
+            scriptureText += word.GetDisplayText() + " ";
+        }
+        return $"{_reference.GetDisplayText()} - {scriptureText.Trim()}";
+    }
+
+    public bool AllWordsHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+        return true;
+    }
+}
